3ef53ee064785e3e9050f32e687b7ae2bed07726
Isaac <156373475+KnockoffofKnockoff@users.noreply.github.com>
2026-08-29 11:47:53 -0400

    added skeletal swerve setup

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,32 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.SwerveDriveSubsystem;
+import yams.mechanisms.swerve.utility.SwerveInputStream;
 
 public class RobotContainer {
+  final CommandXboxController driverXbox = new CommandXboxController(0);
+
+  private final SwerveDriveSubsystem swerve = new SwerveDriveSubsystem();
+
+  private final SwerveInputStream driveAngularVelocity = 
+      swerve.getAngularVelocityStream(
+          driverXbox::getLeftX, 
+          driverXbox::getLeftY, 
+          () -> driverXbox.getRawAxis(2))
+      .withAllianceRelativeControl();
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    swerve.setDefaultCommand(swerve.drive(driveAngularVelocity));
+
+    // zeroing the gyro
+    driverXbox.start().and(driverXbox.back()).onTrue(swerve.zeroGyro());
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
